08025458a95826311c3835a5f5fc6c45cb9e2497
Keyno <kofujiw@gmail.com>
2022-08-29 09:41:23 +0900

    Header の追加

---
+++ pages/portfolio/index.tsx
@@ -0,0 +1,10 @@
+import {NextPage} from "next";
+import Header from "../../components/Header";
+
+const Portfolio: NextPage = () => {
+    return (
+        <Header></Header>
+        )
+}
+
+export default Portfolio;
